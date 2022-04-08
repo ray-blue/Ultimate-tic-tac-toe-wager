3ea93a7c4bb150238b6ef4349fbbacea7fc1730b
ray-blue <surayangobeni882@gmail.com>
2022-04-08 11:04:24 +0200

    tic-tac-toe

---
+++ src/App.js
@@ -0,0 +1,104 @@
+
+import './App.css';
+import { useState } from 'react';
+import Square from './Componets/Square';
+
+
+function App({val, chooseSquare   }) {
+  const [board, setBoard] = useState(["", "","","" ,"", "",""," ",""]);
+   const [player, setPlayer] = useState("X");
+
+const chooseSquare = (square) =>{
+  setBoard(board.map ((val, idx) => {
+    if (idx == square && val == "") {
+      return player;
+    }
+
+return val;
+})
+);
+if (player == "X") {
+  setPlayer("0")
+} else{
+  setPlayer("X")
+}
+};
+
+return (
+  <div className="App">
+    <div className="board">
+      <div className="row">
+        <Square
+        val={board[0]}
+        chooseSquare= { () => {
+          chooseSquare(0);
+        }}
+        />
+       
+  
+    <Square
+      val={board[1]}
+      chooseSquare = { () => {
+        chooseSquare(1);
+      }}
+         />
+
+        
+  
+        
+        <Square
+        val={board[2]}
+        chooseSquare = { () => {
+          chooseSquare(2);
+        }}
+           />
+
+
+
+        
+        </div>
+        <div className="row">
+       
+        <Square
+        val={board[3]}
+        chooseSquare= { () => {
+          chooseSquare(3);
+        }}
+        />
+         <Square
+        val={board[4]}
+        chooseSquare= { () => {
+          chooseSquare(4);
+        }}
+        />
+        <Square
+        val={board[5]}
+        chooseSquare= { () => {
+          chooseSquare(5);
+        }}
+        />
+        </div>
+        </div>
+        <div className="row">
+          <Square
+        val={board[6]}
+        chooseSquare= { () => {
+          chooseSquare(6);
+        }}
+        />
+        <Square
+        val={board[7]}
+        chooseSquare= { () => {
+          chooseSquare(7);
+        }}
+        />
+        <Square
+        val={board[8]}
+        chooseSquare= { () => {
+          chooseSquare(8);
+        }}
+        />
+        </div>
+        </div>
+        
+        export default App;
---
+++ src/Componets/Square.js
@@ -0,0 +1,11 @@
+ import React from 'react'
+ import "../App.css";
+ 
+ export default function Square({val,chooseSquare}) {
+   return (
+   <div className='square'onClick={chooseSquare}>
+       {val}
+   </div>
+   )
+ }
+ 
